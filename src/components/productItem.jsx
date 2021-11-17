14db5f20798721f205c6f09273831c1717d5cc82
michel <michel.sampil@dualbootpartners.com>
2021-11-17 14:20:23 -0300

    react routing v6 ready

--- src/components/productItem.jsx
+++ src/components/productItem.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ListItem = (prop) => {
-    const { productItem } = prop;
-    const [ item, setItem ] = useState(productItem);
+  const { productItem } = prop;
+  const [item, setItem] = useState(productItem);
 
-    return (
-        <div>
-            {item.title}
-        </div>
-    )
-}
+  return (
+    <>
+      <Link to={`${item.id}`} >{item.title}</Link>
+    </>
+  );
+};
 
 export default ListItem;
---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Routes, Route, Link, Outlet } from 'react-router-dom'
+
+const WelcomePage = () => {
+    return (
+    <section>
+        <h1>Welcome!!!</h1>
+        <Link to="new-user"> New User </Link>
+        {/* we can define the nestes routes in only one file approach */}
+        {/* <Routes>
+            <Route path="new-user" element={<p> Welcome, new user! </p>}/>
+        </Routes> */}
+        <Outlet/>
+
+    </section>
+    )
+}
+
+export default WelcomePage;
