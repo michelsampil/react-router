36baf4dc2a22d04be92242b5d596e1e8b1ecab08
michel <michel.sampil@dualbootpartners.com>
2021-11-17 23:48:25 -0300

    basic-item-styles

--- src/components/productItem.jsx
+++ src/components/productItem.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import classes from "./ProductItem.module.css";
 
 const ListItem = (prop) => {
   const { productItem } = prop;
   const [item, setItem] = useState(productItem);
 
   return (
-    <>
-      <Link to={`${item.id}`} >{item.title}</Link>
-    </>
+    <div className={classes.itemCard}>
+      <img className={classes.img} src={item.image} alt={item.title} />
+      <section className={classes.textSection}>
+        <Link to={`${item.id}`}>{item.title}</Link>
+        <p className={classes.text}>{item.subheading}</p>
+        <p className={classes.text}>{Date(item.date)}</p>
+        <button className={classes.button}> Read More </button>
+      </section>
+    </div>
   );
 };
 
