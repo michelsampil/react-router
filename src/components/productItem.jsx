f6ba80992a6c878700639427b42317b466dffe5a
michel <michel.sampil@dualbootpartners.com>
2021-11-17 23:53:46 -0300

    read more fixed

--- src/components/productItem.jsx
+++ src/components/productItem.jsx
@@ -14,7 +14,7 @@ const ListItem = (prop) => {
         <h2>{item.title}</h2>
         <p className={classes.text}>{item.subheading}</p>
         <p className={classes.text}>{Date(item.date)}</p>
-        <Link className={classes.button} to={`${item.id}`}>Read more...</Link>
+        <Link className={classes.button} to={`${item.id}`}>read more...</Link>
       </section>
     </div>
   );
