c7307a1e0f33774f6ce8ae7bcfed1e11ef8b0b8c
michel sampil <michel.sampil@gmail.com>
2023-10-26 18:21:14 -0300

    navigate

--- src/components/productList.jsx
+++ src/components/productList.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 import ProductItem from "./ProductItem.jsx";
 
-const ProductList = (props) => {
-  const { items } = props;
-  console.log("items bro: ", items);
+const ProductList = ({ items }) => {
   const [productItems, setProductItems] = useState(items);
 
   return (
     productItems.length > 0 && (
-      <>
+      <ul>
         {productItems.map((item) => {
           return <ProductItem key={item.id} productItem={item} />;
         })}
-      </>
+      </ul>
     )
   );
 };
